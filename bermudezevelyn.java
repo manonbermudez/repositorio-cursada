5a8bba942c01aa029c1bcfc09ca2828a7f513067
manonbermudez <140336760+manonbermudez@users.noreply.github.com>
2023-08-23 23:04:27 -0300

    Add files via upload

---
+++ bermudezevelyn.java
@@ -0,0 +1,14 @@
+package clase_1;
+
+public class bermudezevelyn{
+
+    public static void main(String[] args) {
+        int numero = 10;
+        float numero2 = numero;
+
+        System.out.println(numero);
+        System.out.println(numero2);
+
+        
+    }
+}
